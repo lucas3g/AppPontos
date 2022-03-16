df31af243c018ffa4cf34f50f61d898712c7e47b
lucassilva080397 <lucassilva080397@gmail.com>
2022-03-16 11:55:16 -0300

    ajustes para o googlemaps funcionar no iOS

--- bio_app_pontos/ios/Runner/AppDelegate.swift
+++ bio_app_pontos/ios/Runner/AppDelegate.swift
@@ -1,5 +1,6 @@
 import UIKit
 import Flutter
+import GoogleMaps
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
@@ -7,6 +8,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDo8_neK-5CVA26bq5iZkq1HzUSWeeHc_Q")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
