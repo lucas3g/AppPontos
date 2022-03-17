834af8cf003c080cea000dfa40777aa507e50bac
lucassilva080397 <lucassilva080397@gmail.com>
2022-03-17 09:50:17 -0300

    teste para ios

--- bio_app_pontos/lib/src/controllers/maps/maps_controller.g.dart
+++ bio_app_pontos/lib/src/controllers/maps/maps_controller.g.dart
@@ -9,81 +9,10 @@ part of 'maps_controller.dart';
 // ignore_for_file: non_constant_identifier_names, unnecessary_brace_in_string_interps, unnecessary_lambdas, prefer_expression_function_bodies, lines_longer_than_80_chars, avoid_as, avoid_annotating_with_dynamic
 
 mixin _$MapsController on _MapsControllerBase, Store {
-  final _$statusAtom = Atom(name: '_MapsControllerBase.status');
-
-  @override
-  MapsStatus get status {
-    _$statusAtom.reportRead();
-    return super.status;
-  }
-
-  @override
-  set status(MapsStatus value) {
-    _$statusAtom.reportWrite(value, super.status, () {
-      super.status = value;
-    });
-  }
-
-  final _$markerPostoAtom = Atom(name: '_MapsControllerBase.markerPosto');
-
-  @override
-  Set<Marker> get markerPosto {
-    _$markerPostoAtom.reportRead();
-    return super.markerPosto;
-  }
-
-  @override
-  set markerPosto(Set<Marker> value) {
-    _$markerPostoAtom.reportWrite(value, super.markerPosto, () {
-      super.markerPosto = value;
-    });
-  }
-
-  final _$kGooglePlexAtom = Atom(name: '_MapsControllerBase.kGooglePlex');
-
-  @override
-  CameraPosition get kGooglePlex {
-    _$kGooglePlexAtom.reportRead();
-    return super.kGooglePlex;
-  }
-
-  @override
-  set kGooglePlex(CameraPosition value) {
-    _$kGooglePlexAtom.reportWrite(value, super.kGooglePlex, () {
-      super.kGooglePlex = value;
-    });
-  }
-
-  final _$abreMapaAsyncAction = AsyncAction('_MapsControllerBase.abreMapa');
-
-  @override
-  Future<void> abreMapa() {
-    return _$abreMapaAsyncAction.run(() => super.abreMapa());
-  }
-
-  final _$navigateToAsyncAction = AsyncAction('_MapsControllerBase.navigateTo');
-
-  @override
-  Future<void> navigateTo(double lat, double lng) {
-    return _$navigateToAsyncAction.run(() => super.navigateTo(lat, lng));
-  }
-
-  final _$goToTheLakeAsyncAction =
-      AsyncAction('_MapsControllerBase.goToTheLake');
-
-  @override
-  Future<void> goToTheLake(
-      {required Completer<GoogleMapController> completer}) {
-    return _$goToTheLakeAsyncAction
-        .run(() => super.goToTheLake(completer: completer));
-  }
-
   @override
   String toString() {
     return '''
-status: ${status},
-markerPosto: ${markerPosto},
-kGooglePlex: ${kGooglePlex}
+
     ''';
   }
 }
--- bio_app_pontos/lib/src/firebase_messaging/custom_firebase_messaging.dart
+++ bio_app_pontos/lib/src/firebase_messaging/custom_firebase_messaging.dart
@@ -1,38 +1,38 @@
-import 'package:bio_app_pontos/src/firebase_messaging/custom_local_notification.dart';
-import 'package:bio_app_pontos/src/utils/constants.dart';
-import 'package:firebase_messaging/firebase_messaging.dart';
-import 'package:flutter/cupertino.dart';
+// import 'package:bio_app_pontos/src/firebase_messaging/custom_local_notification.dart';
+// import 'package:bio_app_pontos/src/utils/constants.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:flutter/cupertino.dart';
 
-class CustomFirebaseMessaging {
-  final CustomLocalNotification _customLocalNotification;
+// class CustomFirebaseMessaging {
+//   final CustomLocalNotification _customLocalNotification;
 
-  CustomFirebaseMessaging._internal(this._customLocalNotification);
+//   CustomFirebaseMessaging._internal(this._customLocalNotification);
 
-  static final CustomFirebaseMessaging _singleton =
-      CustomFirebaseMessaging._internal(CustomLocalNotification());
-  factory CustomFirebaseMessaging() => _singleton;
+//   static final CustomFirebaseMessaging _singleton =
+//       CustomFirebaseMessaging._internal(CustomLocalNotification());
+//   factory CustomFirebaseMessaging() => _singleton;
 
-  Future<void> inicialize() async {
-    await FirebaseMessaging.instance
-        .setForegroundNotificationPresentationOptions(badge: true, sound: true);
+//   Future<void> inicialize() async {
+//     await FirebaseMessaging.instance
+//         .setForegroundNotificationPresentationOptions(badge: true, sound: true);
 
-    await FirebaseMessaging.instance.subscribeToTopic("all");
+//     await FirebaseMessaging.instance.subscribeToTopic("all");
 
-    FirebaseMessaging.onMessage.listen((message) {
-      RemoteNotification? notification = message.notification;
-      AndroidNotification? android = message.notification?.android;
-      if (notification != null && android != null) {
-        _customLocalNotification.androidNotification(notification, android);
-      }
-    });
+//     FirebaseMessaging.onMessage.listen((message) {
+//       RemoteNotification? notification = message.notification;
+//       AndroidNotification? android = message.notification?.android;
+//       if (notification != null && android != null) {
+//         _customLocalNotification.androidNotification(notification, android);
+//       }
+//     });
 
-    FirebaseMessaging.onMessageOpenedApp.listen((message) {
-      if (message.data['goTO'] != null) {
-        Constants.navigatorKey.currentState?.pushNamed(message.data['goTO']);
-      }
-    });
-  }
+//     FirebaseMessaging.onMessageOpenedApp.listen((message) {
+//       if (message.data['goTO'] != null) {
+//         Constants.navigatorKey.currentState?.pushNamed(message.data['goTO']);
+//       }
+//     });
+//   }
 
-  getTokenFirebase() async =>
-      debugPrint(await FirebaseMessaging.instance.getToken());
-}
+//   getTokenFirebase() async =>
+//       debugPrint(await FirebaseMessaging.instance.getToken());
+// }
