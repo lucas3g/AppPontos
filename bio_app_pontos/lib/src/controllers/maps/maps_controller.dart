a3b97b37325eebd15f827d446c55a98771e7c479
lucassilva080397 <lucassilva080397@gmail.com>
2022-03-17 09:54:19 -0300

    voltado config

--- bio_app_pontos/lib/src/controllers/maps/maps_controller.dart
+++ bio_app_pontos/lib/src/controllers/maps/maps_controller.dart
@@ -1,56 +1,56 @@
 import 'dart:async';
 
 import 'package:bio_app_pontos/src/controllers/maps/maps_status.dart';
-// import 'package:bio_app_pontos/src/utils/constants.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:bio_app_pontos/src/utils/constants.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:mobx/mobx.dart';
-// import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart';
 part 'maps_controller.g.dart';
 
 class MapsController = _MapsControllerBase with _$MapsController;
 
 abstract class _MapsControllerBase with Store {
-  // @observable
-  // MapsStatus status = MapsStatus.empty;
+  @observable
+  MapsStatus status = MapsStatus.empty;
 
-  // @observable
-  // Set<Marker> markerPosto = {};
+  @observable
+  Set<Marker> markerPosto = {};
 
-  // @observable
-  // CameraPosition kGooglePlex = CameraPosition(
-  //   target: LatLng(Constants.latitude, Constants.longitude),
-  //   zoom: 18,
-  // );
+  @observable
+  CameraPosition kGooglePlex = CameraPosition(
+    target: LatLng(Constants.latitude, Constants.longitude),
+    zoom: 18,
+  );
 
-  // @action
-  // Future<void> abreMapa() async {
-  //   status = MapsStatus.loading;
-  //   Marker mk = Marker(
-  //     markerId: MarkerId(Constants.idMarker),
-  //     position: LatLng(Constants.latitude, Constants.longitude),
-  //     infoWindow: InfoWindow(
-  //       title: Constants.tituloMarker,
-  //     ),
-  //   );
-  //   markerPosto.add(mk);
-  //   await Future.delayed(Duration(milliseconds: 600));
-  //   status = MapsStatus.success;
-  // }
+  @action
+  Future<void> abreMapa() async {
+    status = MapsStatus.loading;
+    Marker mk = Marker(
+      markerId: MarkerId(Constants.idMarker),
+      position: LatLng(Constants.latitude, Constants.longitude),
+      infoWindow: InfoWindow(
+        title: Constants.tituloMarker,
+      ),
+    );
+    markerPosto.add(mk);
+    await Future.delayed(Duration(milliseconds: 600));
+    status = MapsStatus.success;
+  }
 
-  // @action
-  // Future<void> navigateTo(double lat, double lng) async {
-  //   var uri = Uri.parse("google.navigation:q=$lat,$lng&mode=d");
-  //   if (await canLaunch(uri.toString())) {
-  //     await launch(uri.toString());
-  //   } else {
-  //     throw 'Não pode ser aberto ${uri.toString()}';
-  //   }
-  // }
+  @action
+  Future<void> navigateTo(double lat, double lng) async {
+    var uri = Uri.parse("google.navigation:q=$lat,$lng&mode=d");
+    if (await canLaunch(uri.toString())) {
+      await launch(uri.toString());
+    } else {
+      throw 'Não pode ser aberto ${uri.toString()}';
+    }
+  }
 
-  // @action
-  // Future<void> goToTheLake(
-  //     {required Completer<GoogleMapController> completer}) async {
-  //   final GoogleMapController controller = await completer.future;
-  //   controller.animateCamera(CameraUpdate.newCameraPosition(kGooglePlex));
-  // }
+  @action
+  Future<void> goToTheLake(
+      {required Completer<GoogleMapController> completer}) async {
+    final GoogleMapController controller = await completer.future;
+    controller.animateCamera(CameraUpdate.newCameraPosition(kGooglePlex));
+  }
 }
--- bio_app_pontos/lib/src/pages/dashboard/localization/localization_widget.dart
+++ bio_app_pontos/lib/src/pages/dashboard/localization/localization_widget.dart
@@ -1,79 +1,79 @@
-// import 'dart:async';
+import 'dart:async';
 
-// import 'package:bio_app_pontos/src/configs/global_settings.dart';
-// import 'package:bio_app_pontos/src/controllers/maps/maps_status.dart';
-// import 'package:bio_app_pontos/src/utils/constants.dart';
-// import 'package:bio_app_pontos/src/utils/loading_widget.dart';
-// import 'package:flutter_mobx/flutter_mobx.dart';
-// import 'package:flutter/material.dart';
-// import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:bio_app_pontos/src/configs/global_settings.dart';
+import 'package:bio_app_pontos/src/controllers/maps/maps_status.dart';
+import 'package:bio_app_pontos/src/utils/constants.dart';
+import 'package:bio_app_pontos/src/utils/loading_widget.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-// import '../../../theme/app_theme.dart';
+import '../../../theme/app_theme.dart';
 
-// class LocalizationWidget extends StatefulWidget {
-//   LocalizationWidget({Key? key}) : super(key: key);
+class LocalizationWidget extends StatefulWidget {
+  LocalizationWidget({Key? key}) : super(key: key);
 
-//   @override
-//   State<LocalizationWidget> createState() => _LocalizationWidgetState();
-// }
+  @override
+  State<LocalizationWidget> createState() => _LocalizationWidgetState();
+}
 
-// class _LocalizationWidgetState extends State<LocalizationWidget> {
-//   final mapsController = GlobalSettings().mapsController;
-//   final Completer<GoogleMapController> controllerGoogle = Completer();
+class _LocalizationWidgetState extends State<LocalizationWidget> {
+  final mapsController = GlobalSettings().mapsController;
+  final Completer<GoogleMapController> controllerGoogle = Completer();
 
-//   Future<void> carregaMapa() async {
-//     await mapsController.abreMapa();
-//   }
+  Future<void> carregaMapa() async {
+    await mapsController.abreMapa();
+  }
 
-//   @override
-//   void initState() {
-//     super.initState();
-//     carregaMapa();
-//   }
+  @override
+  void initState() {
+    super.initState();
+    carregaMapa();
+  }
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       body: Observer(builder: (context) {
-//         return mapsController.status == MapsStatus.success
-//             ? GoogleMap(
-//                 mapType: MapType.hybrid,
-//                 initialCameraPosition: mapsController.kGooglePlex,
-//                 zoomControlsEnabled: false,
-//                 markers: mapsController.markerPosto,
-//                 mapToolbarEnabled: false,
-//                 myLocationButtonEnabled: false,
-//                 onMapCreated: (GoogleMapController controller) {
-//                   controllerGoogle.complete(controller);
-//                 },
-//               )
-//             : LoadingWidget(
-//                 size: Size(double.maxFinite, double.maxFinite), radius: 10);
-//       }),
-//       floatingActionButton: Padding(
-//         padding: const EdgeInsets.only(left: 30),
-//         child: Row(
-//           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-//           children: [
-//             FloatingActionButton.extended(
-//               onPressed: () {
-//                 mapsController.navigateTo(
-//                     Constants.latitude, Constants.longitude);
-//               },
-//               label: Text('Para o posto!'),
-//               icon: Icon(Icons.local_gas_station_rounded),
-//               backgroundColor: AppTheme.colors.primary,
-//             ),
-//             FloatingActionButton(
-//               onPressed: () {
-//                 mapsController.goToTheLake(completer: controllerGoogle);
-//               },
-//               backgroundColor: AppTheme.colors.primary,
-//               child: Icon(Icons.my_location_rounded),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Observer(builder: (context) {
+        return mapsController.status == MapsStatus.success
+            ? GoogleMap(
+                mapType: MapType.hybrid,
+                initialCameraPosition: mapsController.kGooglePlex,
+                zoomControlsEnabled: false,
+                markers: mapsController.markerPosto,
+                mapToolbarEnabled: false,
+                myLocationButtonEnabled: false,
+                onMapCreated: (GoogleMapController controller) {
+                  controllerGoogle.complete(controller);
+                },
+              )
+            : LoadingWidget(
+                size: Size(double.maxFinite, double.maxFinite), radius: 10);
+      }),
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.only(left: 30),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            FloatingActionButton.extended(
+              onPressed: () {
+                mapsController.navigateTo(
+                    Constants.latitude, Constants.longitude);
+              },
+              label: Text('Para o posto!'),
+              icon: Icon(Icons.local_gas_station_rounded),
+              backgroundColor: AppTheme.colors.primary,
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                mapsController.goToTheLake(completer: controllerGoogle);
+              },
+              backgroundColor: AppTheme.colors.primary,
+              child: Icon(Icons.my_location_rounded),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
